b99d70c708406dec9ff4fdf5543aba8ec67c503b
martadudka <martakryklevych@gmail.com>
2024-12-21 11:04:30 +0100

    update reverse function

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -1,4 +1,4 @@
-function reverseMutateArray(arr) {
+export function reverseMutateArray(arr) {
   let left = 0;
   let right = arr.length - 1;
   while (left < right) {
@@ -11,23 +11,17 @@ function reverseMutateArray(arr) {
     right--;
   }
 
-  return arr;
 }
-
-console.log(reverseMutateArray([-1, 3, 2, 9]));
 
 export function reverse(arr) {
   let reversedArray = [];
-  for (let i = 0; i < arr.length; i++) {
-    reversedArray[i] = arr[arr.length - 1 - i];
-    // 1st iteration: reversedArray[0] = arr[3 - 1 - 0];
-    // 2nd iteration: reversedArray[1] = arr[3 - 1 - 1];
-    // 3rd iteration: reversedArray[2] = arr[3 - 1 - 2];
+  for(let i = arr.length - 1; i >= 0; i--) {
+    reversedArray.push(arr[i]);
   }
+  
   return reversedArray;
 }
 
-console.log(reverse([3, 2, 9]));
 
 // function reverse(arr) {
 //   return [...arr].reverse();
@@ -37,4 +31,4 @@ console.log(reverse([3, 2, 9]));
 //   return arr.reverse();
 // }
 
-// console.log(reverse([2, 1, 7]))
+// reverse([2, 1, 7])
--- src/reverse/reverse.test.js
+++ src/reverse/reverse.test.js
@@ -1,18 +1,46 @@
-import { test, expect } from "vitest";
-import { reverse } from "./reverse";
+import { test, expect, describe } from "vitest";
+import { reverse, reverseMutateArray } from "./reverse";
 
-test("should return an array with reversed order", () => {
-  expect(reverse([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
+describe("reverseMutateArray", () => {
+  test("should reverse the original array", () => {
+    const originalArr = [1, 2, 3, 4, 5];
+    reverseMutateArray(originalArr);
+    expect(originalArr).toEqual([5, 4, 3, 2, 1]);
+  });
+
+  test("should reverse the original array", () => {
+    const originalArr = [1, 2, 3, 4];
+    reverseMutateArray(originalArr);
+    expect(originalArr).toEqual([4, 3, 2, 1]);
+  });
+
+  // test("should return an empty array when input is empty", () => {
+  //   expect(reverseMutateArray([])).toEqual([]);
+  // });
+
+  // test("should return the same array if it has only one element", () => {
+  //   expect(reverseMutateArray([42])).toEqual([42]);
+  // });
+
+  // test("should handle an array with negative numbers", () => {
+  //   expect(reverseMutateArray([-1, -2, -3, -4, -5])).toEqual([-5, -4, -3, -2, -1]);
+  // });
 });
 
-test("should return an empty array when input is empty", () => {
-  expect(reverse([])).toEqual([]);
-});
+describe("reverse", () => {
+  test("should return an array with reversed order", () => {
+    expect(reverse([1, 2, 3, 4, 5])).toEqual([5, 4, 3, 2, 1]);
+  });
 
-test("should return the same array if it has only one element", () => {
-  expect(reverse([42])).toEqual([42]);
-});
+  test("should return an empty array when input is empty", () => {
+    expect(reverse([])).toEqual([]);
+  });
 
-test("should handle an array with negative numbers", () => {
-  expect(reverse([-1, -2, -3, -4, -5])).toEqual([-5, -4, -3, -2, -1]);
+  test("should return the same array if it has only one element", () => {
+    expect(reverse([42])).toEqual([42]);
+  });
+
+  test("should handle an array with negative numbers", () => {
+    expect(reverse([-1, -2, -3, -4, -5])).toEqual([-5, -4, -3, -2, -1]);
+  });
 });
