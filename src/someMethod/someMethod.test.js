981e376366c52bf6ffea71b2d2893d282e876251
martadudka <martakryklevych@gmail.com>
2025-01-25 16:16:22 +0100

    update someMethod.test.js

--- src/someMethod/someMethod.test.js
+++ src/someMethod/someMethod.test.js
@@ -2,13 +2,19 @@ import { test, expect } from "vitest";
 import { someMethod } from "./someMethod.js";
 
 test("returns true when at least one of the elements in the array meets the condition", () => {
-    const arr = [1, 3, 7, 12];
-    const isGreaterThan10 = (number) => number > 10;
-    expect(someMethod(arr, isGreaterThan10)).toEqual(true);
+  const arr = [1, 3, 7, 12];
+  const isGreaterThan10 = (number) => number > 10;
+  expect(someMethod(arr, isGreaterThan10)).toEqual(true);
 });
 
-test("returns false when none of the elements in the array meets the condition", () => {
+test("returns false if none of the elements in the array meets the condition", () => {
   const arr = [1, 3, 7, -1];
+  const isGreaterThan10 = (number) => number > 10;
+  expect(someMethod(arr, isGreaterThan10)).toEqual(false);
+});
+
+test("returns false when the array is empty", () => {
+  const arr = [];
   const isGreaterThan10 = (number) => number > 10;
   expect(someMethod(arr, isGreaterThan10)).toEqual(false);
 });
